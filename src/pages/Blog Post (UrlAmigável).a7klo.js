8028d9ee1915418abb4a7940b8b15b3b7b8ea135
Raphael Vitor <raphaelvitorlopes@icloud.com>
2024-04-10 09:29:49 -0300

    fix: arrumado link do blog em cada post

--- src/pages/Blog Post (UrlAmigável).a7klo.js
+++ src/pages/Blog Post (UrlAmigável).a7klo.js
@@ -19,7 +19,7 @@ $w("#repeater4").onItemReady(($item, itemData) => {
     $item("#text64").html = `<div id="comp-ltq6m8133__50780342-7e04-4e57-acec-20c2df2dba98" class="wWvZUh" style="--height: 120px; font-size: 14px; color: #404C60;">
         <div id="text-content-comp-ltq6m8133__50780342-7e04-4e57-acec-20c2df2dba98" data-scope="comp-ltqhfl1t__50780342-7e04-4e57-acec-20c2df2dba98" data-repeater-items-indexes="0" class="c9GqVL QxJLC3 comp-ltq6m8133 wixui-rich-text" data-testid="richTextElement">
         ${conteudo}</div></div>`;
-    $item("#linkBlog").link = "/blog/" + itemData.urlAmigavel;
+    $item("#linkBlog").link = "/blog/post/" + itemData.urlAmigavel;
 });
 
 $w.onReady(function () {
